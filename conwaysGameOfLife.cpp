d754b30d6be9f39326557118e0010a84da9de351
Luis Ángel Santos Herrero <luis1337@outlook.es>
2017-05-21 00:35:53 +0200

    Update conwaysGameOfLife.cpp

--- conwaysGameOfLife.cpp
+++ conwaysGameOfLife.cpp
@@ -58,7 +58,7 @@ void display() {
 	for (int x = 0; x < 16; x++) {
 		for (int y = 0; y < 16; y++) {
 			if (pad[x][y] == 1) {
-				cout << "1 ";
+				cout << (char)254u << " ";
 			}
 			else {
 				cout << "  ";
