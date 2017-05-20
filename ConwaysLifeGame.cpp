3dd89343fde5fd059e0a5946d2195db69632afb9
Luis Ángel Santos Herrero <luis1337@outlook.es>
2017-05-21 00:27:57 +0200

    Add files via upload

---
+++ ConwaysLifeGame.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+
+using	namespace std;
+
+void	refresh();
+void	display();
+void	clear();
+void	setup();
+
+int		pad[16][16] = { 0 };
+
+int main() {
+	setup();
+
+	while (true) {
+		refresh();
+		display();
+		Sleep(500);
+		clear();
+	}
+	
+}
+void refresh() {
+
+	for (int x = 0; x < 16; x++) {
+		for (int y = 0; y < 16; y++) {
+			int neighbors = pad[x-1][y-1] + pad[x][y-1] + pad[x+1][y-1] + pad[x-1][y] + pad[x+1][y] + pad[x-1][y+1] + pad[x][y+1] + pad[x+1][y+1];
+
+			if (pad[x][y] == 1) {
+				if (neighbors <= 1) {
+					//MUERE DE SOLEDAD
+					pad[x][y] = 0;
+				}
+				else if (neighbors >= 4) {
+					//MUERE DE SOBREPOBLACIÓN
+					pad[x][y] = 0;
+				}
+				else if (neighbors == 2 || neighbors == 3) {
+					//SOBREVIVE
+					pad[x][y] = 1;
+				}
+			}
+			else {
+				if (neighbors == 3) {
+					//VUELVE A LA VIDA
+					pad[x][y] = 1;
+				}
+			}
+
+		}
+	}
+
+}
+void display() {
+
+	for (int x = 0; x < 16; x++) {
+		for (int y = 0; y < 16; y++) {
+			if (pad[x][y] == 1) {
+				cout << "1 ";
+			}
+			else {
+				cout << "  ";
+			}
+		}
+		cout << endl;
+	}
+
+}
+void clear() {
+	HANDLE                     hStdOut;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD                      count;
+	DWORD                      cellCount;
+	COORD                      homeCoords = { 0, 0 };
+
+	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+	/* Get the number of cells in the current buffer */
+	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
+	cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+	/* Fill the entire buffer with spaces */
+	if (!FillConsoleOutputCharacter(
+		hStdOut,
+		(TCHAR) ' ',
+		cellCount,
+		homeCoords,
+		&count
+	)) return;
+
+	/* Fill the entire buffer with the current colors and attributes */
+	if (!FillConsoleOutputAttribute(
+		hStdOut,
+		csbi.wAttributes,
+		cellCount,
+		homeCoords,
+		&count
+	)) return;
+
+	/* Move the cursor home */
+	SetConsoleCursorPosition(hStdOut, homeCoords);
+}
+void setup() {
+	for (int x = 0; x < 16; x++) {
+		for (int y = 0; y < 16; y++) {
+			int r = rand() % 100;
+			if (r < 49) {
+				pad[x][y] = 1;
+			}
+			else {
+				pad[x][y] = 0;
+			}
+		}
+	}
+}
