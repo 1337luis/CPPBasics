79706d4c4562b8021b7c3c2044f7b54a646ba4d7
Luis Ángel Santos Herrero <luis1337@outlook.es>
2017-06-20 15:42:51 +0200

    Code clear

--- ThreadConway.cpp
+++ ThreadConway.cpp
@@ -11,81 +11,94 @@ void	clear();
 void	setup();
 
 const int	padSize = 16;
-int		pad[padSize][padSize] = { 0 };
+int		    pad[ padSize ][ padSize ] = { 0 };
 
 int main() {
     setup();
 
-    thread m(refresh);
-    thread n(display);
+    thread m( refresh );
+    thread n( display );
     
     m.join();
     n.join();
 
 }
 void refresh() {
-while(true){
-    for (int x = 0; x < padSize; x++) {
-        for (int y = 0; y < padSize; y++) {
-            int neighbors = pad[x - 1][y - 1] + pad[x][y - 1] + pad[x + 1][y - 1] + pad[x - 1][y] + pad[x + 1][y] + pad[x - 1][y + 1] + pad[x][y + 1] + pad[x + 1][y + 1];
+    
+    while( true ){
+        
+        for ( int x = 0; x < padSize; x++ ) {
+            
+            for ( int y = 0; y < padSize; y++ ) {
+                
+                int neighbors = pad[x - 1][y - 1] + pad[x][y - 1] + pad[x + 1][y - 1] + pad[x - 1][y] + pad[x + 1][y] + pad[x - 1][y + 1] + pad[x][y + 1] + pad[x + 1][y + 1];
 
-            if (pad[x][y] == 1) {
-                if (neighbors <= 1) {
-                    //MUERE DE SOLEDAD
-                    pad[x][y] = 0;
+                if ( pad[ x ][ y ] == 1 ) {
+                    if ( neighbors <= 1 ) {
+                        //MUERE DE SOLEDAD
+                        pad[ x ][ y ] = 0;
+                    }
+                    else if ( neighbors >= 4 ) {
+                        //MUERE DE SOBREPOBLACIÓN
+                        pad[ x ][ y ] = 0;
+                    }
+                    else if ( neighbors == 2 || neighbors == 3 ) {
+                        //SOBREVIVE
+                        pad[ x ][ y ] = 1;
+                    }
                 }
-                else if (neighbors >= 4) {
-                    //MUERE DE SOBREPOBLACIÓN
-                    pad[x][y] = 0;
+                else {
+                    if ( neighbors == 3 ) {
+                        //VUELVE A LA VIDA
+                        pad[ x ][  y] = 1;
+                    }
                 }
-                else if (neighbors == 2 || neighbors == 3) {
-                    //SOBREVIVE
-                    pad[x][y] = 1;
-                }
+                
             }
-            else {
-                if (neighbors == 3) {
-                    //VUELVE A LA VIDA
-                    pad[x][y] = 1;
-                }
-            }
-
         }
+        
+        Sleep( 200 );
+        
     }
-    Sleep(200);
 }
-}
+
 void display() {
-while(true){
-    for (int x = 0; x < padSize; x++) {
-        for (int y = 0; y < padSize; y++) {
-            if (pad[x][y] == 1) {
-                cout << (char)254u << " ";
+    
+    while( true ){
+        
+        for ( int x = 0; x < padSize; x++ ) {
+            
+            for ( int y = 0; y < padSize; y++ ) {
+                
+                if ( pad[ x ][ y ] == 1 ) {
+                    cout << (char)254u << " ";
+                }
+                else {
+                    cout << "  ";
+                }
+                
             }
-            else {
-                cout << "  ";
-            }
+            
+            cout << endl;
         }
-        cout << endl;
+        
+        Sleep( 200 );
+        clear();
     }
-    Sleep(200);
-    clear();
-}
 }
 
 void clear() {
-    system("cls");
+    system( "cls" );
 }
 void setup() {
-    for (int x = 0; x < padSize; x++) {
-        for (int y = 0; y < padSize; y++) {
-            int r = rand() % 100;
-            if (r < 49) {
-                pad[x][y] = 1;
-            }
-            else {
-                pad[x][y] = 0;
-            }
+    
+    for ( int x = 0; x < padSize; x++ ) {
+        
+        for ( int y = 0; y < padSize; y++ ) {
+            
+            pad[ x ][ y ] = rand() % 2;
+            
         }
+        
     }
 }
